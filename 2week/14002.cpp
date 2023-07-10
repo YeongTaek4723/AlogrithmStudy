1921efa67850f946aee38a0c4f6ae7133a3983b3
YeongTaek4723 <taks0108@gmail.com>
2023-07-10 20:51:01 +0900

    2week

--- 2week/14002.cpp
+++ 2week/14002.cpp
@@ -2,11 +2,35 @@
 
 using namespace std;
 
+int arr[1000], dp[1000];
+
 int main(){
     ios_base::sync_with_stdio(false); 
     cin.tie(NULL);                      
     cout.tie(NULL);
 
-    
+    int N;
+    cin >> N;
+
+
+    for(int i = 0; i < N; i++){
+        cin >> arr[i];
+    }
+
+    for(int i = 0; i < N; i++){
+        dp[i] = 1;
+
+        for(int j = 0; j < i; j++){
+            if(arr[i] > arr[j] && dp[i] < dp[j] + 1)    dp[i] = dp[j] + 1;
+        }
+    }
+
+    int max = 0;
+    for(int i = 0; i < N; i++){
+        if(max < dp[i])     max = dp[i];
+    }
+
+    cout << max << endl;
+
     return 0;
 }
